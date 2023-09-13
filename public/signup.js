2be262212f0054538d2f6f9ea43ff352143b8438
tusharkarlawar <tusharkarlawar95@gmail.com>
2023-09-13 09:27:17 +0530

    modified

--- public/signup.js
+++ public/signup.js
@@ -19,7 +19,7 @@ button.addEventListener("click", async(e)=>{
         if(adduser.data.newUser){
             console.log("User data saved in database");
             alert(adduser.data.msg);
-            //window.location.href="./login.html";
+            window.location.href="./login.html";
         }else{
             console.log("User already exists");
         }
